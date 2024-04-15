a8eb8fc09b47fa6e7078025337404c3a5988e8cb
Sindre Gulbrandsen <67903160+SindreGulbrandsen@users.noreply.github.com>
2024-04-15 20:29:20 +0200

    Update Game.kt

--- Game.kt
+++ Game.kt
@@ -1,14 +1,24 @@
 package com.example.paag_multi.Classes
 
-class Game {
-    var output = mutableListOf<String>("Question 1", "Question 2 *p*")
-    var players = mutableListOf<Player>(Player("Player 1"), Player("Player 2"))
+class Game (raw_output: MutableList<String> = mutableListOf()){
+    var output = mutableListOf<Question>()
+    var players = mutableListOf<Player>()
     var current = 0
     val rounds = output.size
+    var raw_output = mutableListOf<String>()
+
+    init {
+        this.raw_output = raw_output
+        //raw_output = mutableListOf("Question 1", "Question 2 *p*", "Question 3", "Question 4", "Question 5", "Question 6 *p*")
+        players = mutableListOf(Player("Player 1"), Player("Player 2"), Player("Player 3"), Player("Player 4"), Player("Player 5"))
+        raw_output.shuffle()
+        raw_output.add("Game Over")
+        output.add(Question(raw_output[current],mutableListOf<Player>(players.random())))
+    }
 
     fun printQuestion(): String{
         try{
-            return output[current]
+            return output[current].questionText
         }catch(e: Error){
             return "Error: Dont find the question"
         }
@@ -16,17 +26,28 @@ class Game {
     fun nextQuestion(){
         if (!checkGameEnd()){
             current++
+            output.add(Question(raw_output[current], mutableListOf(players.random())))
         }
     }
     fun prevQuestion(){
-        if(current < 0){
+        if(current > 0){
             current--
         }
     }
     fun checkGameEnd(): Boolean{
-        if (current >= output.size){
+        if (current >= raw_output.size - 1){
             return true
         }
         return false
     }
+    fun addPlayer(player: Player){
+        players.add((player))
+    }
+    fun removePlayer(playerName: String) {
+        val playerToRemove = players.indexOfFirst { it.getName() == playerName }
+        if (playerToRemove != -1) {
+            players.removeAt(playerToRemove)
+        }
+    }
+
 }
