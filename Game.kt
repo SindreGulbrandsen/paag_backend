a45a11c87a2824275d4cce395f27c81ac6425dc9
Sindre Gulbrandsen <67903160+SindreGulbrandsen@users.noreply.github.com>
2024-04-19 17:05:54 +0200

    Update Game.kt

--- Game.kt
+++ Game.kt
@@ -10,7 +10,7 @@ class Game (raw_output: MutableList<String> = mutableListOf()){
     init {
         this.raw_output = raw_output
         //raw_output = mutableListOf("Question 1", "Question 2 *p*", "Question 3", "Question 4", "Question 5", "Question 6 *p*")
-        players = mutableListOf(Player("Player 1"), Player("Player 2"), Player("Player 3"), Player("Player 4"), Player("Player 5"))
+        players = mutableListOf(Player("Player 1"), Player("Player 2"))
         raw_output.shuffle()
         raw_output.add("Game Over")
         output.add(Question(raw_output[current],players))
@@ -34,12 +34,8 @@ class Game (raw_output: MutableList<String> = mutableListOf()){
             current--
         }
     }
-    fun checkGameEnd(): Boolean{
-        if (current >= raw_output.size - 1){
-            return true
-        }
-        return false
-    }
+    fun checkGameEnd(): Boolean = current >= raw_output.size - 1
+
     fun addPlayer(player: Player){
         players.add((player))
     }
